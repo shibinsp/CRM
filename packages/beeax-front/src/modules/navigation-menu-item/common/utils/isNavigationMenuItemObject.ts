e155532fe8fab840ae687f15df1c1bbf081207ac
shibin <shibinsp43@gmail.com>
2026-06-28 14:01:03 +0530

    Add packages/beeax-front/src/modules/navigation-menu-item/common/utils/isNavigationMenuItemObject.ts

---
+++ packages/beeax-front/src/modules/navigation-menu-item/common/utils/isNavigationMenuItemObject.ts
@@ -0,0 +1,6 @@
+import { NavigationMenuItemType } from 'beeax-shared/types';
+import { type NavigationMenuItem } from '~/generated-metadata/graphql';
+
+export const isNavigationMenuItemObject = (
+  item: Pick<NavigationMenuItem, 'type'>,
+) => item.type === NavigationMenuItemType.OBJECT;
