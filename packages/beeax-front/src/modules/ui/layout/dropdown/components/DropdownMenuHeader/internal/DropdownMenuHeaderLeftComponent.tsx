fcabda409d0aaf3df391de8154da268383595fd3
shibin <shibinsp43@gmail.com>
2026-06-28 14:07:02 +0530

    Add packages/beeax-front/src/modules/ui/layout/dropdown/components/DropdownMenuHeader/internal/DropdownMenuHeaderLeftComponent.tsx

---
+++ packages/beeax-front/src/modules/ui/layout/dropdown/components/DropdownMenuHeader/internal/DropdownMenuHeaderLeftComponent.tsx
@@ -0,0 +1,65 @@
+import { type MouseEvent, type ReactElement, useContext } from 'react';
+import { styled } from '@linaria/react';
+import { type Avatar, type AvatarProps } from 'beeax-ui/data-display';
+import { type IconComponent } from 'beeax-ui/icon';
+import { LightIconButton } from 'beeax-ui/input';
+import { ThemeContext, themeCssVariables } from 'beeax-ui/theme-constants';
+
+const StyledNonClickableStartIcon = styled.div`
+  align-items: center;
+  background: transparent;
+  border: none;
+
+  display: flex;
+  flex-direction: row;
+
+  font-family: ${themeCssVariables.font.family};
+  font-weight: ${themeCssVariables.font.weight.regular};
+  gap: ${themeCssVariables.spacing[1]};
+  height: ${themeCssVariables.spacing[6]};
+
+  justify-content: center;
+  white-space: nowrap;
+  width: ${themeCssVariables.spacing[6]};
+`;
+
+const StyledAvatarWrapper = styled.div`
+  padding: ${themeCssVariables.spacing[1]};
+`;
+
+export const DropdownMenuHeaderLeftComponent = ({
+  onClick,
+  ...props
+}: { onClick?: (event: MouseEvent<HTMLButtonElement>) => void } & (
+  | { Icon: IconComponent }
+  | {
+      Avatar: ReactElement<AvatarProps, typeof Avatar>;
+    }
+  | Record<never, never>
+)) => {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <>
+      {'Icon' in props &&
+        (onClick ? (
+          <LightIconButton
+            Icon={props.Icon}
+            accent="tertiary"
+            size="small"
+            onClick={onClick}
+          />
+        ) : (
+          <StyledNonClickableStartIcon>
+            <props.Icon
+              size={theme.icon.size.sm}
+              color={theme.font.color.tertiary}
+            />
+          </StyledNonClickableStartIcon>
+        ))}
+
+      {'Avatar' in props && (
+        <StyledAvatarWrapper>{props.Avatar}</StyledAvatarWrapper>
+      )}
+    </>
+  );
+};
