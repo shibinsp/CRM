93571f71fdaabee86e8b4ce0a4b6b05fc1028cbe
shibin <shibinsp43@gmail.com>
2026-06-28 14:09:03 +0530

    Add packages/beeax-front/src/pages/settings/accounts/SettingsAccounts.tsx

---
+++ packages/beeax-front/src/pages/settings/accounts/SettingsAccounts.tsx
@@ -0,0 +1,51 @@
+import { SettingsAccountsBlocklistSection } from '@/settings/accounts/components/SettingsAccountsBlocklistSection';
+import { SettingsAccountsConnectedAccountsListCard } from '@/settings/accounts/components/SettingsAccountsConnectedAccountsListCard';
+import { SettingsAccountsSettingsSection } from '@/settings/accounts/components/SettingsAccountsSettingsSection';
+import { useMyConnectedAccounts } from '@/settings/accounts/hooks/useMyConnectedAccounts';
+import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
+import { SettingsSectionSkeletonLoader } from '@/settings/components/SettingsSectionSkeletonLoader';
+import { SettingsPageLayout } from '@/settings/components/layout/SettingsPageLayout';
+import { useLingui } from '@lingui/react/macro';
+import { SettingsPath } from 'beeax-shared/types';
+import { getSettingsPath } from 'beeax-shared/utils';
+import { H2Title } from 'beeax-ui/typography';
+import { Section } from 'beeax-ui/layout';
+
+export const SettingsAccounts = () => {
+  const { t } = useLingui();
+
+  const { accounts: allAccounts, loading } = useMyConnectedAccounts();
+
+  return (
+    <SettingsPageLayout
+      title={t`Account`}
+      links={[
+        {
+          children: t`User`,
+          href: getSettingsPath(SettingsPath.ProfilePage),
+        },
+        { children: t`Account` },
+      ]}
+    >
+      <SettingsPageContainer>
+        {loading ? (
+          <SettingsSectionSkeletonLoader />
+        ) : (
+          <>
+            <Section>
+              <H2Title
+                title={t`Connected accounts`}
+                description={t`Manage your internet accounts.`}
+              />
+              <SettingsAccountsConnectedAccountsListCard
+                accounts={allAccounts}
+              />
+            </Section>
+            <SettingsAccountsBlocklistSection />
+            <SettingsAccountsSettingsSection />
+          </>
+        )}
+      </SettingsPageContainer>
+    </SettingsPageLayout>
+  );
+};
