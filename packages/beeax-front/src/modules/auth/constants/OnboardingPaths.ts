d8f20f84935d4190d325e3c402f42746e222037c
shibin <shibinsp43@gmail.com>
2026-06-28 14:00:11 +0530

    Add packages/beeax-front/src/modules/auth/constants/OnboardingPaths.ts

---
+++ packages/beeax-front/src/modules/auth/constants/OnboardingPaths.ts
@@ -0,0 +1,15 @@
+import { AppPath } from 'beeax-shared/types';
+
+export const ONBOARDING_PATHS = [
+  AppPath.WorkspaceActivation,
+  AppPath.WorkspaceActivationV2,
+  AppPath.CreateProfile,
+  AppPath.CreateProfileV2,
+  AppPath.SyncEmails,
+  AppPath.SyncEmailsV2,
+  AppPath.InviteTeam,
+  AppPath.PlanRequired,
+  AppPath.PlanRequiredSuccess,
+  AppPath.BookCallDecision,
+  AppPath.BookCall,
+];
