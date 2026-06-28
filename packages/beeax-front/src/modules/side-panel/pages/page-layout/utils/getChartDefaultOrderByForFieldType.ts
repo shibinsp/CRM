608d1b1a43b10e1a0b5b5ed4fe9c42e2b0d3a87f
shibin <shibinsp43@gmail.com>
2026-06-28 14:06:23 +0530

    Add packages/beeax-front/src/modules/side-panel/pages/page-layout/utils/getChartDefaultOrderByForFieldType.ts

---
+++ packages/beeax-front/src/modules/side-panel/pages/page-layout/utils/getChartDefaultOrderByForFieldType.ts
@@ -0,0 +1,12 @@
+import { FieldMetadataType } from 'beeax-shared/types';
+import { GraphOrderBy } from '~/generated-metadata/graphql';
+
+export const getChartDefaultOrderByForFieldType = (
+  fieldType: FieldMetadataType,
+): GraphOrderBy => {
+  const isSelectField = fieldType === FieldMetadataType.SELECT;
+
+  return isSelectField
+    ? GraphOrderBy.FIELD_POSITION_ASC
+    : GraphOrderBy.FIELD_ASC;
+};
