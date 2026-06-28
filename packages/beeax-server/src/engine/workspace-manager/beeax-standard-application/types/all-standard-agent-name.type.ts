fe1555e81e9945e11162f893008b10614a8ad4b2
shibin <shibinsp43@gmail.com>
2026-06-28 14:18:24 +0530

    Add packages/beeax-server/src/engine/workspace-manager/beeax-standard-application/types/all-standard-agent-name.type.ts

---
+++ packages/beeax-server/src/engine/workspace-manager/beeax-standard-application/types/all-standard-agent-name.type.ts
@@ -0,0 +1,3 @@
+import { type STANDARD_AGENT } from 'src/engine/workspace-manager/beeax-standard-application/constants/standard-agent.constant';
+
+export type AllStandardAgentName = keyof typeof STANDARD_AGENT;
