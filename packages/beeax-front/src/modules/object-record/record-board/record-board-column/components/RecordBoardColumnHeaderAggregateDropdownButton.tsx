63a1f8d46efac93d1f29462f1a6f1de66dce67af
shibin <shibinsp43@gmail.com>
2026-06-28 14:01:58 +0530

    Add packages/beeax-front/src/modules/object-record/record-board/record-board-column/components/RecordBoardColumnHeaderAggregateDropdownButton.tsx

---
+++ packages/beeax-front/src/modules/object-record/record-board/record-board-column/components/RecordBoardColumnHeaderAggregateDropdownButton.tsx
@@ -0,0 +1,59 @@
+import { StyledHeaderDropdownButton } from '@/ui/layout/dropdown/components/StyledHeaderDropdownButton';
+import { isDropdownOpenComponentState } from '@/ui/layout/dropdown/states/isDropdownOpenComponentState';
+import { useAtomComponentStateValue } from '@/ui/utilities/state/jotai/hooks/useAtomComponentStateValue';
+import { styled } from '@linaria/react';
+import { type Nullable } from 'beeax-shared/types';
+import { isDefined } from 'beeax-shared/utils';
+import { Tag } from 'beeax-ui/data-display';
+import { AppTooltip, TooltipDelay } from 'beeax-ui/surfaces';
+
+const StyledTagContainer = styled.div`
+  width: 100%;
+`;
+
+const StyledHeaderContainer = styled.div`
+  > * {
+    padding: 0;
+  }
+`;
+
+export const RecordBoardColumnHeaderAggregateDropdownButton = ({
+  dropdownId,
+  value,
+  tooltip,
+}: {
+  dropdownId: string;
+  value?: Nullable<string | number>;
+  tooltip?: Nullable<string>;
+}) => {
+  const isDropdownOpen = useAtomComponentStateValue(
+    isDropdownOpenComponentState,
+    dropdownId,
+  );
+
+  return (
+    <StyledHeaderContainer>
+      <StyledHeaderDropdownButton id={dropdownId} isUnfolded={isDropdownOpen}>
+        <>
+          <StyledTagContainer>
+            <Tag
+              text={isDefined(value) ? value.toString() : '-'}
+              color="transparent"
+              weight="regular"
+            />
+          </StyledTagContainer>
+          {!isDropdownOpen && (
+            <AppTooltip
+              anchorSelect={`#${dropdownId}`}
+              content={tooltip ?? ''}
+              noArrow
+              place="right"
+              positionStrategy="fixed"
+              delay={TooltipDelay.mediumDelay}
+            />
+          )}
+        </>
+      </StyledHeaderDropdownButton>
+    </StyledHeaderContainer>
+  );
+};
