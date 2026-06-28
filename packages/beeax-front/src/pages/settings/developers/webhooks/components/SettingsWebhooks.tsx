b516e74af4ee5b553130c8a06cd42fc711ef233e
shibin <shibinsp43@gmail.com>
2026-06-28 14:09:21 +0530

    Add packages/beeax-front/src/pages/settings/developers/webhooks/components/SettingsWebhooks.tsx

---
+++ packages/beeax-front/src/pages/settings/developers/webhooks/components/SettingsWebhooks.tsx
@@ -0,0 +1,68 @@
+import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
+import { SettingsWebhooksTable } from '@/settings/developers/components/SettingsWebhooksTable';
+import { SettingsPageLayout } from '@/settings/components/layout/SettingsPageLayout';
+import { useIsMobile } from '@/ui/utilities/responsive/hooks/useIsMobile';
+import { styled } from '@linaria/react';
+import { Trans, useLingui } from '@lingui/react/macro';
+import { SettingsPath } from 'beeax-shared/types';
+import { getSettingsPath } from 'beeax-shared/utils';
+import { IconPlus } from 'beeax-ui/icon';
+import { H2Title } from 'beeax-ui/typography';
+import { Button } from 'beeax-ui/input';
+import { Section } from 'beeax-ui/layout';
+import { MOBILE_VIEWPORT, themeCssVariables } from 'beeax-ui/theme-constants';
+
+const StyledButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-top: ${themeCssVariables.spacing[2]};
+  @media (max-width: ${MOBILE_VIEWPORT}px) {
+    padding-top: ${themeCssVariables.spacing[5]};
+  }
+`;
+
+const StyledContainer = styled.div<{ isMobile: boolean }>`
+  display: flex;
+  flex-direction: column;
+  gap: ${themeCssVariables.spacing[2]};
+  overflow: hidden;
+`;
+
+export const SettingsWebhooks = () => {
+  const isMobile = useIsMobile();
+  const { t } = useLingui();
+
+  return (
+    <SettingsPageLayout
+      title={t`Webhooks`}
+      links={[
+        {
+          children: <Trans>Workspace</Trans>,
+          href: getSettingsPath(SettingsPath.General),
+        },
+        { children: <Trans>Webhooks</Trans> },
+      ]}
+    >
+      <SettingsPageContainer>
+        <StyledContainer isMobile={isMobile}>
+          <Section>
+            <H2Title
+              title={t`Webhooks`}
+              description={t`Establish Webhook endpoints for notifications on asynchronous events.`}
+            />
+            <SettingsWebhooksTable />
+            <StyledButtonContainer>
+              <Button
+                Icon={IconPlus}
+                title={t`Create Webhook`}
+                size="small"
+                variant="secondary"
+                to={getSettingsPath(SettingsPath.NewWebhook)}
+              />
+            </StyledButtonContainer>
+          </Section>
+        </StyledContainer>
+      </SettingsPageContainer>
+    </SettingsPageLayout>
+  );
+};
