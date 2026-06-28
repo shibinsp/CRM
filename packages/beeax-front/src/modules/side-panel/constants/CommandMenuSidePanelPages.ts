434866deb0c2d8095c67722dac71de92c163265f
shibin <shibinsp43@gmail.com>
2026-06-28 14:06:02 +0530

    Add packages/beeax-front/src/modules/side-panel/constants/CommandMenuSidePanelPages.ts

---
+++ packages/beeax-front/src/modules/side-panel/constants/CommandMenuSidePanelPages.ts
@@ -0,0 +1,7 @@
+import { SidePanelPages } from 'beeax-shared/types';
+
+export const COMMAND_MENU_SIDE_PANEL_PAGES = [
+  SidePanelPages.CommandMenuDisplay,
+  SidePanelPages.CommandMenuEdit,
+  SidePanelPages.SearchRecords,
+];
