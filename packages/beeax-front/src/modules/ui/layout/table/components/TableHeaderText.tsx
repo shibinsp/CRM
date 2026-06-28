3f566158f9864413eb343aca49d17a113cf816cd
shibin <shibinsp43@gmail.com>
2026-06-28 14:07:15 +0530

    Add packages/beeax-front/src/modules/ui/layout/table/components/TableHeaderText.tsx

---
+++ packages/beeax-front/src/modules/ui/layout/table/components/TableHeaderText.tsx
@@ -0,0 +1,8 @@
+import { styled } from '@linaria/react';
+import { themeCssVariables } from 'beeax-ui/theme-constants';
+
+const StyledTableHeaderText = styled.div`
+  padding-bottom: ${themeCssVariables.spacing['0.5']};
+`;
+
+export { StyledTableHeaderText as TableHeaderText };
