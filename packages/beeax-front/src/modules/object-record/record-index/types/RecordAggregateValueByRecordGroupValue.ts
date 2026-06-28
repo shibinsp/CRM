a2349b39fe8790205abea28877e18e2b486961c3
shibin <shibinsp43@gmail.com>
2026-06-28 14:02:51 +0530

    Add packages/beeax-front/src/modules/object-record/record-index/types/RecordAggregateValueByRecordGroupValue.ts

---
+++ packages/beeax-front/src/modules/object-record/record-index/types/RecordAggregateValueByRecordGroupValue.ts
@@ -0,0 +1,6 @@
+import { type Nullable } from 'beeax-shared/types';
+
+export type RecordAggregateValueByRecordGroupValue = {
+  recordGroupValue: Nullable<string>;
+  recordAggregateValue: Nullable<string | number>;
+};
