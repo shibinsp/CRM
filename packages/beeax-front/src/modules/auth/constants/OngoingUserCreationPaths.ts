ccbc28917e66d819d17d6800c08a386ca689819b
shibin <shibinsp43@gmail.com>
2026-06-28 14:00:11 +0530

    Add packages/beeax-front/src/modules/auth/constants/OngoingUserCreationPaths.ts

---
+++ packages/beeax-front/src/modules/auth/constants/OngoingUserCreationPaths.ts
@@ -0,0 +1,9 @@
+import { AppPath } from 'beeax-shared/types';
+
+export const ONGOING_USER_CREATION_PATHS = [
+  AppPath.Invite,
+  AppPath.SignInUp,
+  AppPath.SignInUpV2,
+  AppPath.VerifyEmail,
+  AppPath.Verify,
+];
