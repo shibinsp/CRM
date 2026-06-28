c733f7efe41b4de573d7e87b996e67ac3262302f
shibin <shibinsp43@gmail.com>
2026-06-28 14:18:22 +0530

    Add packages/beeax-server/src/engine/workspace-manager/beeax-standard-application/beeax-standard-application.module.ts

---
+++ packages/beeax-server/src/engine/workspace-manager/beeax-standard-application/beeax-standard-application.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+
+import { ApplicationModule } from 'src/engine/core-modules/application/application.module';
+import { TwentyConfigModule } from 'src/engine/core-modules/beeax-config/beeax-config.module';
+import { WorkspaceManyOrAllFlatEntityMapsCacheModule } from 'src/engine/metadata-modules/flat-entity/services/workspace-many-or-all-flat-entity-maps-cache.module';
+import { GlobalWorkspaceDataSourceModule } from 'src/engine/beeax-orm/global-workspace-datasource/global-workspace-datasource.module';
+import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
+import { WorkspaceMigrationModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration.module';
+
+import { TwentyStandardApplicationService } from './services/beeax-standard-application.service';
+
+@Module({
+  providers: [TwentyStandardApplicationService],
+  imports: [
+    ApplicationModule,
+    TwentyConfigModule,
+    WorkspaceCacheModule,
+    WorkspaceMigrationModule,
+    GlobalWorkspaceDataSourceModule,
+    WorkspaceManyOrAllFlatEntityMapsCacheModule,
+  ],
+  exports: [TwentyStandardApplicationService],
+})
+export class TwentyStandardApplicationModule {}
