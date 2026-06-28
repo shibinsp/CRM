d7c45f8e333ae4eb6876bf4a2d6d87c068164964
shibin <shibinsp43@gmail.com>
2026-06-28 14:17:26 +0530

    Add packages/beeax-server/src/engine/metadata-modules/object-metadata/utils/build-description-for-relation-field-on-from-field.util.ts

---
+++ packages/beeax-server/src/engine/metadata-modules/object-metadata/utils/build-description-for-relation-field-on-from-field.util.ts
@@ -0,0 +1,12 @@
+import { capitalize } from 'beeax-shared/utils';
+export const buildDescriptionForRelationFieldMetadataOnFromField = ({
+  relationObjectMetadataNamePlural,
+  targetObjectLabelSingular,
+}: {
+  relationObjectMetadataNamePlural: string;
+  targetObjectLabelSingular: string;
+}) => {
+  const description = `${capitalize(relationObjectMetadataNamePlural)} tied to the ${targetObjectLabelSingular}`;
+
+  return { description };
+};
