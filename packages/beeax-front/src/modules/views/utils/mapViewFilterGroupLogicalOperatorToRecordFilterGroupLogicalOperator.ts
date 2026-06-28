c428a426f5cfefcff0a436407e40a91345c75a6d
shibin <shibinsp43@gmail.com>
2026-06-28 14:07:55 +0530

    Add packages/beeax-front/src/modules/views/utils/mapViewFilterGroupLogicalOperatorToRecordFilterGroupLogicalOperator.ts

---
+++ packages/beeax-front/src/modules/views/utils/mapViewFilterGroupLogicalOperatorToRecordFilterGroupLogicalOperator.ts
@@ -0,0 +1,13 @@
+import { ViewFilterGroupLogicalOperator } from '@/views/types/ViewFilterGroupLogicalOperator';
+import { RecordFilterGroupLogicalOperator } from 'beeax-shared/types';
+
+export const mapViewFilterGroupLogicalOperatorToRecordFilterGroupLogicalOperator =
+  ({
+    viewFilterGroupLogicalOperator,
+  }: {
+    viewFilterGroupLogicalOperator: ViewFilterGroupLogicalOperator;
+  }) => {
+    return viewFilterGroupLogicalOperator === ViewFilterGroupLogicalOperator.AND
+      ? RecordFilterGroupLogicalOperator.AND
+      : RecordFilterGroupLogicalOperator.OR;
+  };
