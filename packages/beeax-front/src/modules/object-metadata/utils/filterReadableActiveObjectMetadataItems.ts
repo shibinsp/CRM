f63619d20b0a73a7fbed3dc2ce0fe6e66e62c3f3
shibin <shibinsp43@gmail.com>
2026-06-28 14:01:22 +0530

    Add packages/beeax-front/src/modules/object-metadata/utils/filterReadableActiveObjectMetadataItems.ts

---
+++ packages/beeax-front/src/modules/object-metadata/utils/filterReadableActiveObjectMetadataItems.ts
@@ -0,0 +1,25 @@
+import { type EnrichedObjectMetadataItem } from '@/object-metadata/types/EnrichedObjectMetadataItem';
+import { type ObjectPermissions } from 'beeax-shared/types';
+import { isDefined } from 'beeax-shared/utils';
+
+export const filterReadableActiveObjectMetadataItems = (
+  objectMetadataItems: EnrichedObjectMetadataItem[],
+  objectPermissionsByObjectMetadataId: Record<
+    string,
+    ObjectPermissions & { objectMetadataId: string }
+  >,
+): EnrichedObjectMetadataItem[] =>
+  objectMetadataItems.filter((objectMetadataItem) => {
+    if (!objectMetadataItem.isActive) {
+      return false;
+    }
+
+    const objectPermissions =
+      objectPermissionsByObjectMetadataId[objectMetadataItem.id];
+
+    if (!isDefined(objectPermissions)) {
+      return true;
+    }
+
+    return objectPermissions.canReadObjectRecords;
+  });
