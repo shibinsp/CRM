c17945491dcf1d06131b99276e89f1b5cbe1a3ec
shibin <shibinsp43@gmail.com>
2026-06-28 14:01:03 +0530

    Add packages/beeax-front/src/modules/navigation-menu-item/common/utils/getObjectMetadataIdsInDraft.ts

---
+++ packages/beeax-front/src/modules/navigation-menu-item/common/utils/getObjectMetadataIdsInDraft.ts
@@ -0,0 +1,18 @@
+import { NavigationMenuItemType } from 'beeax-shared/types';
+import { isDefined } from 'beeax-shared/utils';
+import { type NavigationMenuItem } from '~/generated-metadata/graphql';
+
+export const getObjectMetadataIdsInDraft = (
+  draft: Pick<NavigationMenuItem, 'type' | 'targetObjectMetadataId'>[],
+): Set<string> =>
+  draft.reduce<Set<string>>((ids, item) => {
+    if (item.type !== NavigationMenuItemType.OBJECT) {
+      return ids;
+    }
+
+    if (isDefined(item.targetObjectMetadataId)) {
+      ids.add(item.targetObjectMetadataId);
+    }
+
+    return ids;
+  }, new Set<string>());
