ec436b7281f5f61bee69a2ec53b4454de5179034
shibin <shibinsp43@gmail.com>
2026-06-28 14:00:07 +0530

    Add packages/beeax-front/src/modules/apollo/types/coreObjectNamesToDeleteOnRelationDetach.ts

---
+++ packages/beeax-front/src/modules/apollo/types/coreObjectNamesToDeleteOnRelationDetach.ts
@@ -0,0 +1,7 @@
+import { CoreObjectNameSingular } from 'beeax-shared/types';
+
+export const CORE_OBJECT_NAMES_TO_DELETE_ON_TRIGGER_RELATION_DETACH = [
+  CoreObjectNameSingular.NoteTarget,
+  CoreObjectNameSingular.TaskTarget,
+  CoreObjectNameSingular.Comment,
+];
